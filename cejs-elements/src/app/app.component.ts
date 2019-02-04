0c0661834df2ff087b49088ed39b4f7eeaf8e8a1
Jesús Cuesta <jesuscuesta83@gmail.com>
2019-02-04 13:11:30 +0100

    feat(angular-elements): initial

---
+++ cejs-elements/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'atsistemas-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cejs-elements';
+}
